94f5c8c59cf6bd16e731a42ab9ed83fab25526ae
Richard Ruston <raruston@earthstormsoftware.com>
2023-02-22 21:35:44 +0100

    Add JumpData

--- EliteFIPProtocol/GameData.cs
+++ EliteFIPProtocol/GameData.cs
@@ -4,7 +4,8 @@
         Status,
         Target,
         Location,
-        Navigation        
+        Navigation,
+        Jump
     }
 
     public class GameData {
---
+++ EliteFIPProtocol/JumpData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EliteFIPProtocol {
+    public class JumpData {
+        public DateTime LastUpdate { get; set; }
+        public string OriginSystemId { get; set; }
+        public string OriginSystemName { get; set; }        
+        public string DestinationSystemId { get; set; }
+        public string DestinationSystemName { get; set; }
+        public string DestinationSystemClass { get; set; }
+        public double JumpDistance { get; init; }
+        public double FuelUsed { get; init; }
+
+        public static FIPPacket CreateFIPPacket(JumpData jumpData) {
+
+            GameData gameData = new GameData();
+            gameData.Type = GameDataType.Jump.ToString();
+            gameData.Data = JsonSerializer.Serialize(jumpData);
+
+            FIPPacket packet = new FIPPacket();
+            packet.PacketType = PacketType.GameData.ToString();
+            packet.Payload = JsonSerializer.Serialize(gameData);
+            return packet;
+        }
+    }
+}
